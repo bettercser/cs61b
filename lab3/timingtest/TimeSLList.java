bf8f248730baa275db1dc72599488b9ae0700d26
bettercser <huhackjz@gmail.com>
2024-10-11 11:54:01 +0800

    SLList timing test

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -5,6 +5,7 @@ import edu.princeton.cs.algs4.Stopwatch;
  * Created by hug.
  */
 public class TimeSLList {
+    private static final int M = 10000;
     private static void printTimingTable(AList<Integer> Ns, AList<Double> times, AList<Integer> opCounts) {
         System.out.printf("%12s %12s %12s %12s\n", "N", "time (s)", "# ops", "microsec/op");
         System.out.printf("------------------------------------------------------------\n");
@@ -23,6 +24,28 @@ public class TimeSLList {
 
     public static void timeGetLast() {
         // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+        SLList<Integer> integerSLList = new SLList<>();
+        int start = 1000;
+        while(true){
+
+            for(int i = 0; i < start; i++){
+                integerSLList.addLast(i);
+            }
+            Stopwatch stopwatch = new Stopwatch();
+            for(int i = 0; i < M; i++){
+                integerSLList.getLast();
+            }
+            double time = stopwatch.elapsedTime();
+            Ns.addLast(start);
+            times.addLast(time);
+            opCounts.addLast(M);
+            start *= 2;
+            if(start > 128000) break;
+        }
+        printTimingTable(Ns, times, opCounts);
     }
 
 }
